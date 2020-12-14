068543ce5d86be1b078108f3d675666f94e415c4
David Robertson <david.m.robertson1@gmail.com>
2020-12-14 00:30:56 +0000

    Day 11 part 2
    Some more debug etc in here because I misread the rules at first.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,4 +1,7 @@
-#[derive(Clone, Eq, PartialEq)]
+use std::fmt;
+use std::fmt::{Debug, Formatter};
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum Tile {
     Floor,
     EmptySeat,
@@ -6,6 +9,13 @@ enum Tile {
 }
 
 impl Tile {
+    fn char(&self) -> char {
+        match self {
+            Tile::Floor => '.',
+            Tile::EmptySeat => 'L',
+            Tile::OccupiedSeat => '#',
+        }
+    }
     fn parse(c: char) -> Self {
         match c {
             '.' => Self::Floor,
@@ -16,6 +26,17 @@ impl Tile {
     }
 }
 
+const DIRECTION_VECTORS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 #[derive(PartialEq, Eq, Clone)]
 struct Layout {
     width: usize,
@@ -24,7 +45,11 @@ struct Layout {
 }
 
 impl Layout {
-    fn parse<'a>(mut input: impl Iterator<Item = &'a str>) -> Self {
+    fn parse(input: &str) -> Self {
+        Self::from_rows(input.split("\n"))
+    }
+
+    fn from_rows<'a>(mut input: impl Iterator<Item = &'a str>) -> Self {
         let first = input.next().unwrap();
         let width = first.len();
         let mut grid: Vec<_> = first.chars().map(Tile::parse).collect();
@@ -40,16 +65,28 @@ impl Layout {
         }
     }
 
-    fn iterate(&self) -> Self {
+    fn get(&self, x: isize, y: isize) -> Option<&Tile> {
+        if 0 <= x && x < self.width as isize && 0 <= y && y < self.height as isize {
+            Some(&self.grid[self.width * (y as usize) + x as usize])
+        } else {
+            None
+        }
+    }
+
+    fn iterate(
+        &self,
+        count_occupation: fn(&Layout, isize, isize) -> u8,
+        occupation_leave_threshold: u8,
+    ) -> Self {
         let mut next = Layout {
             width: self.width,
             height: self.height,
             grid: self.grid.to_vec(),
         };
-        for x in 0..self.width {
-            for y in 0..self.height {
+        for y in 0..self.height {
+            for x in 0..self.width {
                 let index = y * self.width + x;
-                let occupied = self.occupied_adjacent_seats(x as isize, y as isize);
+                let occupied = count_occupation(self, x as isize, y as isize);
                 let next_tile = match self.grid[index] {
                     Tile::Floor => Tile::Floor,
                     Tile::EmptySeat => {
@@ -60,7 +97,7 @@ impl Layout {
                         }
                     }
                     Tile::OccupiedSeat => {
-                        if occupied >= 4 {
+                        if occupied >= occupation_leave_threshold {
                             Tile::EmptySeat
                         } else {
                             Tile::OccupiedSeat
@@ -73,30 +110,37 @@ impl Layout {
         next
     }
 
-    fn occupied_adjacent_seats(&self, x: isize, y: isize) -> u8 {
+    fn adjacent_occupied_seats(&self, x: isize, y: isize) -> u8 {
         // Not very happy with the casts going on here. Maybe there's a better way.
-        let occupied_at = |(dx, dy): &(isize, isize)| -> u8 {
-            let x = x + dx;
-            let y = y + dy;
-            let occupied =
-                if 0 <= x && x < self.width as isize && 0 <= y && y < self.height as isize {
-                    self.grid[self.width * (y as usize) + x as usize] == Tile::OccupiedSeat
-                } else {
-                    false
-                };
-            occupied as u8
+        let occupied_at = |(dx, dy): &(isize, isize)| -> bool {
+            self.get(x + dx, y + dy) == Some(&Tile::OccupiedSeat)
         };
-        let offsets: [(isize, isize); 8] = [
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-        ];
-        offsets.iter().map(occupied_at).sum()
+        DIRECTION_VECTORS
+            .iter()
+            .map(occupied_at)
+            .map(|b| b as u8)
+            .sum()
+    }
+
+    fn visible_occupied_seats(&self, x: isize, y: isize) -> u8 {
+        let occupied_visible_in = |(dx, dy): &(isize, isize)| -> bool {
+            let (mut x, mut y) = (x, y);
+            loop {
+                x += dx;
+                y += dy;
+                match self.get(x, y) {
+                    None => return false,
+                    Some(&Tile::OccupiedSeat) => return true,
+                    Some(&Tile::EmptySeat) => return false,
+                    Some(&Tile::Floor) => continue,
+                }
+            }
+        };
+        DIRECTION_VECTORS
+            .iter()
+            .map(occupied_visible_in)
+            .map(|b| b as u8)
+            .sum()
     }
 
     fn count(&self, t: Tile) -> usize {
@@ -104,10 +148,29 @@ impl Layout {
     }
 }
 
-fn find_steady_state(layout: &Layout) -> Layout {
+impl Debug for Layout {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut output = String::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                output.push(self.grid[y * self.width + x].char());
+            }
+            if y != self.height - 1 {
+                output.push('\n');
+            }
+        }
+        f.write_str(&output)
+    }
+}
+
+fn find_steady_state(
+    layout: &Layout,
+    count_occupation: fn(&Layout, isize, isize) -> u8,
+    occupation_leave_threshold: u8,
+) -> Layout {
     let mut layout = layout.clone();
     loop {
-        let next = layout.iterate();
+        let next = layout.iterate(count_occupation, occupation_leave_threshold);
         if layout == next {
             return next;
         }
@@ -117,18 +180,18 @@ fn find_steady_state(layout: &Layout) -> Layout {
 
 fn main() {
     let input: Vec<_> = aoc_2020::problem_lines().collect();
-    let start = Layout::parse(input.iter().map(String::as_str));
-    let steady = find_steady_state(&start);
+    let start = Layout::from_rows(input.iter().map(String::as_str));
+    let steady = find_steady_state(&start, Layout::adjacent_occupied_seats, 4);
     println!("{}", steady.count(Tile::OccupiedSeat));
+
+    let steady2 = find_steady_state(&start, Layout::visible_occupied_seats, 5);
+    println!("{}", steady2.count(Tile::OccupiedSeat));
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn example() {
-        let layout = "\
+    const STARTING_LAYOUT: &str = "\
 L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
@@ -139,8 +202,62 @@ L.LLLLL.LL
 LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL";
-        let start = Layout::parse(layout.split("\n"));
-        let steady = find_steady_state(&start);
+
+    #[test]
+    fn example_1() {
+        let start = Layout::parse(STARTING_LAYOUT);
+        let steady = find_steady_state(&start, Layout::adjacent_occupied_seats, 4);
         assert_eq!(steady.count(Tile::OccupiedSeat), 37);
+    }
+
+    #[test]
+    fn example_2() {
+        let start = Layout::parse(STARTING_LAYOUT);
+
+        let step1 = Layout::parse(
+            "\
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##",
+        );
+        assert_eq!(step1, start.iterate(Layout::visible_occupied_seats, 5));
+        let step2 = Layout::parse(
+            "\
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#",
+        );
+        assert_eq!(step2, step1.iterate(Layout::visible_occupied_seats, 5));
+        let step3 = Layout::parse(
+            "\
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#",
+        );
+        assert_eq!(step3, step2.iterate(Layout::visible_occupied_seats, 5));
+
+        let steady = find_steady_state(&start, Layout::visible_occupied_seats, 5);
+        assert_eq!(steady.count(Tile::OccupiedSeat), 26);
     }
 }
